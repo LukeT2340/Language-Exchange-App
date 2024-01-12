d5120a54f812afdfda252864bfc0fc3cec1edc32
Luke Thompson <lukethompson220@gmail.com>
2024-01-13 08:00:34 +1100

    Initial Commit

---
+++ LangX/LangXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangXApp.swift
+//  LangX
+//
+//  Created by Luke Thompson on 13/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct LangXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
